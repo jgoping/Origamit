b991aa611b405f97caa5e00cf86df966b6427c7a
XOPHELIOUS <johnefillips@gmail.com>
2017-09-16 16:47:17 -0400

    Fixed camera permission

--- app/src/main/java/com/example/justin/testingvisualizer/MainActivity.java
+++ app/src/main/java/com/example/justin/testingvisualizer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.justin.testingvisualizer;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.Image;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,14 +11,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static android.R.attr.bitmap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -55,9 +62,35 @@ public class MainActivity extends AppCompatActivity {
 
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
             String imageFileName = "JPEG_" + timeStamp + "_";
-            MediaStore.Images.Media.insertImage(getContentResolver(), imageBitmap, imageFileName, "Origamit");
+
+
+            //MediaStore.Images.Media.insertImage(getContentResolver(), imageBitmap, imageFileName, "Origamit");
             Thread t = new Thread(new runnable());
             t.start();
         }
     }
+
+    void saveImage(Bitmap imageBitmap) {
+        File filename;
+        try {
+            String path = Environment.getExternalStorageDirectory().toString();
+
+            new File(path + "/folder/subfolder").mkdirs();
+            filename = new File(path + "/folder/subfolder/image.jpg");
+
+            FileOutputStream out = new FileOutputStream(filename);
+
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+
+            MediaStore.Images.Media.insertImage(getContentResolver(), filename.getAbsolutePath(), filename.getName(), filename.getName());
+
+            Toast.makeText(getApplicationContext(), "File is Saved in  " + filename, Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
