c76c5c21748dca9541f9269ebad4a644ca789625
jgoping <32006038+jgoping@users.noreply.github.com>
2017-09-16 14:39:31 -0400

    Cleaned up code.

--- app/src/main/java/com/example/justin/testingvisualizer/MainActivity.java
+++ app/src/main/java/com/example/justin/testingvisualizer/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Thread t = new Thread(new runnable(this));
+        Thread t = new Thread(new runnable());
         t.start();
 
     }
--- app/src/main/java/com/example/justin/testingvisualizer/runnable.java
+++ app/src/main/java/com/example/justin/testingvisualizer/runnable.java
@@ -1,27 +1,15 @@
 package com.example.justin.testingvisualizer;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.ArrayList;
 
 import org.json.*;
-
-import static android.R.attr.src;
 
 /**
  * Created by Justin on 2017-09-16.
@@ -30,14 +18,6 @@ import static android.R.attr.src;
 public class runnable extends AppCompatActivity implements Runnable {
 
     Thread runner;
-    MainActivity mainView;
-
-    private static final int PERMISSION_REQUEST_CODE = 1;
-
-
-    public runnable(MainActivity mV) {
-        mainView = mV;
-    }
 
     public void run() {
         try  {
@@ -45,29 +25,17 @@ public class runnable extends AppCompatActivity implements Runnable {
             VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
             service.setApiKey("99e49c0dc9f6aed1839753a66f3b9d7865537e4d");
 
-            /*
-            File folder = new File("/storage");
-            String[] listOfFiles = folder.list();
-
-            for (String path : listOfFiles) {
-                System.out.println(path);
-            }
-            */
-
-
 
             System.out.println("Classify an image");
             ClassifyImagesOptions options = new ClassifyImagesOptions.Builder()
-                    .images(new File("/storage/0123-4567/Frog.jpg")) //"/storage/0123-4567/Dog.jpg"
+                    .images(new File("/storage/0123-4567/Frog.jpg"))
                     .build();
             VisualClassification result = service.classify(options).execute();
-            System.out.println(result);
-            System.out.println("ifjdsifjdsof");
+            //System.out.println(result);
 
 
             String s = result.toString();
-            System.out.println(s);
-            System.out.println("ifjdsifjdsof2");
+            //System.out.println(s);
 
             JSONObject obj = new JSONObject(s);
             JSONArray arr = obj.getJSONArray("images");
@@ -76,7 +44,7 @@ public class runnable extends AppCompatActivity implements Runnable {
             obj = new JSONObject(arr.getString(0));
             arr = obj.getJSONArray("classes");
             obj = new JSONObject(arr.getString(0));
-            ArrayList<String> names = new ArrayList<String>();
+            ArrayList<String> names = new ArrayList<>();
             names.add(obj.getString("class"));
             obj = new JSONObject(arr.getString(1));
             names.add(obj.getString("class"));
@@ -84,11 +52,6 @@ public class runnable extends AppCompatActivity implements Runnable {
             names.add(obj.getString("class"));
             System.out.println(names);
 
-            /*
-            for (int i = 0; i < arr.length(); i++)
-                System.out.println(arr.get(i));
-                */
-            //System.out.println(arr.get(0));
 
         } catch (Exception e) {
             e.printStackTrace();
